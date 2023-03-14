4c9ab9653f300c19b4ceeeef02b381533b6d91f0
banhang <lenhata33@gmail.com>
2023-03-14 16:25:30 +0700

    first commit

---
+++ AppBanHang2/app/src/main/java/com/example/appbanhang/utils/Utils.java
@@ -0,0 +1,11 @@
+package com.example.appbanhang.utils;
+
+import com.example.appbanhang.model.GioHang;
+
+import java.util.List;
+
+public class Utils {
+    public static final String BASE_URL = "http://192.168.3.105/nuocep/";
+    public static List<GioHang> manggiohang;
+
+}
